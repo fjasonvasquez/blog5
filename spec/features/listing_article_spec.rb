59dfc5cce2cd8182196cfd9c7bd9a29845f7f2a2
fjasonvasquez <fjvasquez@aol.com>
2017-02-07 00:21:28 -0800

    implemented article listing views and display

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -14,9 +14,8 @@ RSpec.feature "Listing Articles" do
 		expect(page).to have_content(@article1.body)
 		expect(page).to have_content(@article2.title)
 		expect(page).to have_content(@article2.body)
+		expect(page).to have_link(@article1.title)
+		expect(page).to have_link(@article2.title)
+
 	end
 end
-
-# create 2 articles to display
-# list the 2 articles
-# expect both article titles and bodies to be present
